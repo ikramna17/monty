ff638a2a1e27e22014c5a0853171aaba4a76829f
ikram17 <ikramnassih1017@gmail.com>
2023-09-21 13:33:02 -0700

    allll

--- file_tools.c
+++ file_tools.c
@@ -1,84 +1,178 @@
 #include "monty.h"
 
-
 /**
- *main - entry point
- *@argc: arguments count
- *@argv: list of arguments
- *Return: always 0
+ *open_file - opens a file
+ *@file_name: the file namepath
+ *Return: void
  */
 
-int main(int argc, char *argv[])
+void open_file(char *file_name)
 {
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+	FILE *fd = fopen(file_name, "r");
 
-	open_file(argv[1]);
-	free_nodes();
-	return (0);
+	if (file_name == NULL || fd == NULL)
+		err_ikram(2, file_name);
+
+	read_file(fd);
+	fclose(fd);
 }
 
 /**
- *create_node - Creates a node.
- *@n: Num to go inside the node.
- *Return: Upon sucess a pointer to the node. Otherwise NULL.
+ *read_file - reads a file
+ *@fd: pointer to file descriptor
+ *Return: void
  */
-stack_t *create_node(int n)
-{
-	stack_t *node;
 
-	node = malloc(sizeof(stack_t));
-	if (node == NULL)
+void read_file(FILE *fd)
+{
+	int line_number, format = 0;
+	char *buffer = NULL;
+	size_t len = 0;
+
+	for (line_number = 1; getline(&buffer, &len, fd) != -1; line_number++)
+	{
+		format = parse_line(buffer, line_number, format);
+	}
+
+	free(buffer);
+}
+
+/**
+ *parse_line - Separates each line into tokens to determine
+ *which function to call
+ *@buffer: line from the file
+ *@line_number: line num
+ *@format:  storage format. If 0 Nodes will be entered as a stack.
+ *if 1 nodes will be entered as a queue.
+ *Return: Returns 0 if the opcode is stack. 1 if queue.
+ */
+
+int parse_line(char *buffer, int line_number, int format)
+{
+	char *opcode, *value;
+	const char *delim = "\n ";
+
+	if (buffer == NULL)
 		err_ikram(4);
-	node->next = NULL;
-	node->prev = NULL;
-	node->n = n;
-	return (node);
+
+	opcode = strtok(buffer, delim);
+	if (opcode == NULL)
+		return (format);
+	value = strtok(NULL, delim);
+
+	if (strcmp(opcode, "stack") == 0)
+		return (0);
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+
+	find_func(opcode, value, line_number, format);
+	return (format);
 }
 
 /**
- *free_nodes - Frees nodes in the stack.
+ *find_func - find the appropriate function for the opcode
+ *@opcode: opcode
+ *@value: argument of opcode
+ *@format:  storage format. If 0 Nodes will be entered as a stack.
+ *@ln: line num
+ *if 1 nodes will be entered as a queue.
+ *Return: void
  */
-void free_nodes(void)
+void find_func(char *opcode, char *value, int ln, int format)
 {
-	stack_t *tmp;
+	int i;
+	int flag;
 
-	if (head == NULL)
+	instruction_t func_list[] = {
+		{ "push", add_to_stack
+		},
+		{ "pall", print_stack
+		},
+		{ "pint", print_top
+		},
+		{ "pop", pop_top
+		},
+		{ "nop", nop
+		},
+		{ "swap", swap_nodes
+		},
+		{ "add", add_nodes
+		},
+		{ "sub", sub_nodes
+		},
+		{ "div", div_nodes
+		},
+		{ "mul", mul_nodes
+		},
+		{ "mod", mod_nodes
+		},
+		{ "pchar", print_char
+		},
+		{ "pstr", print_str
+		},
+		{ "rotl", rotl
+		},
+		{ "rotr", rotr
+		},
+		{
+			NULL, NULL
+		}
+	};
+
+	if (opcode[0] == '#')
 		return;
 
-	while (head != NULL)
+	for (flag = 1, i = 0; func_list[i].opcode != NULL; i++)
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
+		if (strcmp(opcode, func_list[i].opcode) == 0)
+		{
+			call_fun(func_list[i].f, opcode, value, ln, format);
+			flag = 0;
+		}
 	}
+
+	if (flag == 1)
+		err_ikram(3, ln, opcode);
 }
 
 /**
- *add_to_queue - Adds a node to the queue.
- *@new_node: Pointer to the new node.
- *@ln: line num of the opcode.
+ *call_fun - Calls the required function.
+ *@func: Pointer to the function that is about to be called.
+ *@op: string representing the opcode.
+ *@val: string representing a numeric value.
+ *@ln: line num for the instruction.
+ *@format: Format specifier. If 0 Nodes will be entered as a stack.
+ *if 1 nodes will be entered as a queue.
  */
-void add_to_queue(stack_t **new_node, __attribute__((unused)) unsigned int ln)
+void call_fun(op_func func, char *op, char *val, int ln, int format)
 {
-	stack_t *tmp;
+	stack_t * node;
+	int flag;
+	int i;
 
-	if (new_node == NULL || *new_node == NULL)
-		exit(EXIT_FAILURE);
-	if (head == NULL)
+	flag = 1;
+	if (strcmp(op, "push") == 0)
 	{
-		head = *new_node;
-		return;
+		if (val != NULL && val[0] == '-')
+		{
+			val = val + 1;
+			flag = -1;
+		}
+
+		if (val == NULL)
+			err_ikram(5, ln);
+		for (i = 0; val[i] != '\0'; i++)
+		{
+			if (isdigit(val[i]) == 0)
+				err_ikram(5, ln);
+		}
+
+		node = create_node(atoi(val) *flag);
+		if (format == 0)
+			func(&node, ln);
+		if (format == 1)
+			add_to_queue(&node, ln);
 	}
-
-	tmp = head;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
-	tmp->next = *new_node;
-	(*new_node)->prev = tmp;
-
+	else
+		func(&head, ln);
 }
